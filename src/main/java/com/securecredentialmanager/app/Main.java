5a3440d79286127d6088b4471ef7e90ddb2eae3c
Ntshepiseng macuphe <ntmaccpt@student.wethinkcode.co.za>
2026-06-29 10:13:09 +0200

    database: just tested our database connection and it's working

--- src/main/java/com/securecredentialmanager/app/Main.java
+++ src/main/java/com/securecredentialmanager/app/Main.java
@@ -1,4 +1,9 @@
 package com.securecredentialmanager.app;
 
+import com.securecredentialmanager.database.DatabaseConnection;
+
 public class Main {
+    public static void main(String[] args) {
+        DatabaseConnection.getConnection();
+    }
 }
--- src/main/java/com/securecredentialmanager/database/DatabaseConnection.java
+++ src/main/java/com/securecredentialmanager/database/DatabaseConnection.java
@@ -1,4 +1,29 @@
 package com.securecredentialmanager.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/secure_credential_manager";
+
+    private static final String USER = "scm_user";
+    private static final String PASSWORD = "scm_password";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Database connected successfully!");
+            } catch (SQLException e) {
+                System.out.println("Database connection failed!");
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 }
