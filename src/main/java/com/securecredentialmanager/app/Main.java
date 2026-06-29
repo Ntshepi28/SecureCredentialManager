a593ec90c5da45acc7c61182381aa4d3927d51be
Ntshepiseng macuphe <ntmaccpt@student.wethinkcode.co.za>
2026-06-29 11:03:24 +0200

    update: just added the basic things that may be needed on the user side

--- src/main/java/com/securecredentialmanager/app/Main.java
+++ src/main/java/com/securecredentialmanager/app/Main.java
@@ -1,9 +1,30 @@
 package com.securecredentialmanager.app;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import com.securecredentialmanager.database.DatabaseConnection;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        Label label = new Label("Secure Credential Manager");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("SCM - Login System Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
         DatabaseConnection.getConnection();
+        launch(args);
     }
 }
--- src/main/java/com/securecredentialmanager/models/User.java
+++ src/main/java/com/securecredentialmanager/models/User.java
@@ -1,4 +1,54 @@
 package com.securecredentialmanager.models;
 
 public class User {
+
+    private int Id;
+    private String Username;
+    private String Email;
+    private String PasswordHash;
+
+    public User(int id, String username, String email, String passwordHash) {
+        Id = id;
+        Username = username;
+        Email = email;
+        PasswordHash = passwordHash;
+    }
+
+    public User(String username, String email, String passwordHash) {
+        Username = username;
+        Email = email;
+        PasswordHash = passwordHash;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        this.Id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        this.Username = username;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+
+    public String getPasswordHash() {
+        return PasswordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.PasswordHash = passwordHash;
+    }
 }
