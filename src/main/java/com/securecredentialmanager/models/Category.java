330eb7d1769c88e2c4869aa23595de23f57aa361
Nthsepiseng <ntshepisengmacuphe@gmail.com>
2026-09-16 16:49:11 +0200

    feat: added categories getters and setters based on the table

--- src/main/java/com/securecredentialmanager/models/Category.java
+++ src/main/java/com/securecredentialmanager/models/Category.java
@@ -1,4 +1,53 @@
 package com.securecredentialmanager.models;
 
+import java.time.LocalDateTime;
+
 public class Category {
+
+    private long id;
+    private long userId;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Category() {
+    }
+
+    public Category(long id, long userId, String name, LocalDateTime createdAt) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
