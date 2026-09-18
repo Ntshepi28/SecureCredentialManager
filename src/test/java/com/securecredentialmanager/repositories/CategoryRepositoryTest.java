3f962b799dd0a54167d2845b2116195227afbdf1
Nthsepiseng <ntshepisengmacuphe@gmail.com>
2026-09-18 10:56:22 +0200

    Test: implemented test that deletes a category

--- src/test/java/com/securecredentialmanager/repositories/CategoryRepositoryTest.java
+++ src/test/java/com/securecredentialmanager/repositories/CategoryRepositoryTest.java
@@ -1,4 +1,56 @@
 package com.securecredentialmanager.repositories;
 
+import com.securecredentialmanager.models.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CategoryRepositoryTest {
+    private CategoryRepository categoryRepository;
+
+    @BeforeEach
+    void setUp(){
+        categoryRepository = new CategoryRepository();
+
+        categoryRepository.deleteCategory(1001);
+    }
+
+    @Test
+    void shouldSaveAndFindCategory(){
+        Category category = new Category();
+        category.setId(1001);
+        category.setUserId(10);
+        category.setName("Anathi");
+
+        boolean saved = categoryRepository.saveCategory(category);
+
+        assertTrue(saved);
+
+        Category foundCategory = categoryRepository.findByName(10, "Anathi");
+
+        assertNotNull(foundCategory);
+        assertEquals(1001, foundCategory.getId());
+        assertEquals(10, foundCategory.getUserId());
+        assertEquals("Anathi", foundCategory.getName());
+        assertNotNull(foundCategory.getCreatedAt());
+    }
+
+    @Test
+    void shouldDeleteCategory(){
+        Category category = new Category();
+        category.setId(1001);
+        category.setUserId(10);
+        category.setName("Anathi");
+
+        categoryRepository.saveCategory(category);
+
+        boolean deleted = categoryRepository.deleteCategory(1001);
+
+        assertTrue(deleted);
+
+        Category foundCategory = categoryRepository.findByName(10, "Anathi");
+
+        assertNull(foundCategory);
+    }
 }
