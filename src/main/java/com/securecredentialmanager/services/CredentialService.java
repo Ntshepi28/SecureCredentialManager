cbfb6439d1a9520b8ba40d24bff817104e804210
Nthsepiseng <ntshepisengmacuphe@gmail.com>
2026-09-21 17:10:15 +0200

    feat(service): Add CredentialService it handles the user's saved credentials fro different plartforms

--- src/main/java/com/securecredentialmanager/services/CredentialService.java
+++ src/main/java/com/securecredentialmanager/services/CredentialService.java
@@ -1,4 +1,64 @@
 package com.securecredentialmanager.services;
 
+import com.securecredentialmanager.models.Credential;
+import com.securecredentialmanager.repositories.CredentialRepository;
+
 public class CredentialService {
+
+    private final CredentialRepository credentialRepository;
+
+    public CredentialService(){
+        this.credentialRepository = new CredentialRepository();
+    }
+
+    public boolean saveCredential(Credential credential){
+        if (credential == null){
+            return false;
+        }
+
+        if (credential.getUserId() <= 0){
+            return false;
+        }
+
+        if (credential.getServiceName() == null
+               || credential.getServiceName().isBlank()){
+            return false;
+        }
+
+        if (credential.getLoginUsername() == null
+              || credential.getLoginUsername().isBlank()){
+            return false;
+        }
+
+        if (credential.getEncryptedPassword() == null
+               || credential.getEncryptedPassword().isBlank()){
+            return false;
+        }
+
+        return credentialRepository.saveCredential(credential);
+    }
+
+    public Credential findCredential(long id){
+        if (id <= 0){
+            return null;
+        }
+
+        return credentialRepository.findById((int) id);
+    }
+
+    public Credential findByServiceName(int userId, String serviceName){
+        if (userId <= 0 || serviceName == null || serviceName.isBlank()){
+            return null;
+        }
+
+        return credentialRepository.findByServiceName(userId, serviceName);
+    }
+
+    public boolean deleteCredential(long id){
+        if (id <= 0){
+            return false;
+        }
+
+        return credentialRepository.deleteCredentials((int) id);
+    }
 }
