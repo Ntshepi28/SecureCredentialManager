fa7060177f13c741a3ba1cd094643990909a928f
Nthsepiseng <ntshepisengmacuphe@gmail.com>
2026-09-22 14:28:54 +0200

    feat(controllers): add RegistrationController class it deals with the registration of a user

--- src/main/java/com/securecredentialmanager/controllers/RegistrationController.java
+++ src/main/java/com/securecredentialmanager/controllers/RegistrationController.java
@@ -1,4 +1,46 @@
 package com.securecredentialmanager.controllers;
 
+import com.securecredentialmanager.services.AuthenticationService;
+
 public class RegistrationController {
+
+    private final AuthenticationService authenticationService;
+
+    public RegistrationController(){
+        this.authenticationService = new AuthenticationService();
+    }
+
+    public boolean register(String username, String email, String password){
+        if (username == null || username.isBlank()){
+            return false;
+        }
+
+        if (email == null || email.isBlank()){
+            return false;
+        }
+
+        if (password == null || password.isBlank()){
+            return false;
+        }
+
+        return authenticationService.register(username, email, password);
+    }
+
+    public boolean usernameExists(String username){
+
+        if (username == null || username.isBlank()){
+            return false;
+        }
+
+        return authenticationService.usernameExists(username);
+    }
+
+    public boolean emailExists(String email){
+
+        if (email == null || email.isBlank()){
+            return false;
+        }
+
+        return authenticationService.emailExists(email);
+    }
 }
