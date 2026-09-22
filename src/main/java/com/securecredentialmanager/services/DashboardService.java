c53ef45a84e86705ccd52ecfae2f0b2c94ad0918
Nthsepiseng <ntshepisengmacuphe@gmail.com>
2026-09-22 10:05:05 +0200

    feat(service): Add DashboardService that will be the ui of what will be seeing by the user containing so of the information they will need to look at

--- src/main/java/com/securecredentialmanager/services/DashboardService.java
+++ src/main/java/com/securecredentialmanager/services/DashboardService.java
@@ -1,4 +1,30 @@
 package com.securecredentialmanager.services;
 
+import com.securecredentialmanager.models.AuditLogs;
+import com.securecredentialmanager.models.BackupHistory;
+import com.securecredentialmanager.models.User;
+import com.securecredentialmanager.models.UserSettings;
+import com.securecredentialmanager.repositories.AuditLogsRepository;
+import com.securecredentialmanager.repositories.BackupHistoryRepository;
+import com.securecredentialmanager.repositories.CredentialRepository;
+import com.securecredentialmanager.repositories.UserRepository;
+import com.securecredentialmanager.repositories.UserSettingsRepository;
+
 public class DashboardService {
+
+    private final UserRepository userRepository;
+    private final CredentialRepository credentialRepository;
+    private final AuditLogsRepository auditLogsRepository;
+    private final BackupHistoryRepository backupHistoryRepository;
+    private final UserSettingsRepository userSettingsRepository;
+    private final PasswordStrengthService passwordStrengthService;
+
+    public DashboardService(){
+        this.userRepository = new UserRepository();
+        this.credentialRepository = new CredentialRepository();
+        this.auditLogsRepository = new AuditLogsRepository();
+        this.backupHistoryRepository = new BackupHistoryRepository();
+        this.userSettingsRepository = new UserSettingsRepository();
+        this.passwordStrengthService = new PasswordStrengthService();
+    }
 }
