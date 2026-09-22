7c46c2cffaabc0dfd76efadd685372b7d63487c3
Nthsepiseng <ntshepisengmacuphe@gmail.com>
2026-09-22 12:55:58 +0200

    feat(service): add SecurityLogService class it checks the logs user on what thwy get up to and the type of device they are using also their ipAddress

--- src/main/java/com/securecredentialmanager/services/SecurityLogService.java
+++ src/main/java/com/securecredentialmanager/services/SecurityLogService.java
@@ -1,4 +1,48 @@
 package com.securecredentialmanager.services;
 
+import com.securecredentialmanager.models.AuditLogs;
+import com.securecredentialmanager.repositories.AuditLogsRepository;
+
 public class SecurityLogService {
+
+    private final AuditLogsRepository auditLogsRepository;
+
+    public SecurityLogService(){
+        this.auditLogsRepository = new AuditLogsRepository();
+    }
+
+    public boolean logAction(int userId, String action, String description, String ipAddress,
+                             String deviceName){
+
+        if (userId <= 0 || action == null || action.isBlank()){
+            return false;
+        }
+
+        AuditLogs auditLogs = new AuditLogs();
+
+        auditLogs.setUserId(userId);
+        auditLogs.setAction(action);
+        auditLogs.setDescription(description);
+        auditLogs.setIpAddress(ipAddress);
+        auditLogs.setDeviceName(deviceName);
+
+        return auditLogsRepository.saveAuditLogs(auditLogs);
+    }
+
+    public AuditLogs getLatestLog(int userId){
+
+        if (userId <= 0){
+            return  null;
+        }
+
+        return auditLogsRepository.findLatestByUserId(userId);
+    }
+
+    public AuditLogs getLog(long id){
+        if (id <= 0){
+            return null;
+        }
+
+        return auditLogsRepository.findById(id);
+    }
 }
