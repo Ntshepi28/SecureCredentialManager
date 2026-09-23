f665e04c2b29af425145426d7e36b93e127257d6
Nthsepiseng <ntshepisengmacuphe@gmail.com>
2026-09-23 14:21:18 +0200

    feat(controllers): add BackupView it handles the application of a backedup information by a user

---
+++ src/main/java/com/securecredentialmanager/ui/BackupView.java
@@ -0,0 +1,14 @@
+package com.securecredentialmanager.ui;
+
+import com.securecredentialmanager.controllers.BackupController;
+import com.securecredentialmanager.models.BackupHistory;
+import com.securecredentialmanager.models.User;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class Backupview {
+
+    
+}
